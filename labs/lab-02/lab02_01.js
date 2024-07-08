40ae376012628c1cdbe971771f26ba68d299ba3e
tha <tha@axon.com>
2024-07-08 21:31:05 +0700

    Update lab 2.1

--- labs/lab-02/lab02_01.js
+++ labs/lab-02/lab02_01.js
@@ -5,11 +5,12 @@ let weight = reader.question("your weight: ");
 const bmi = weight/(height*height);
 console.log(`Your BMI: ${bmi}`);
 
-if (bmi < 18.5)
+if (bmi < 18.5){
     console.log("Underweight");
-else if (bmi<= 24.9)
+} else if (bmi<= 24.9){
     console.log ("Normal weight");
-else if (bmi<=29.9 )
+} else if (bmi<=29.9 ){
     console.log ("Overweight");
-else
+} else {
     console.log("Obesity");
+}   
--- lessons/day-01/hello.js
+++ lessons/day-01/hello.js
@@ -1,2 +1,13 @@
 console.log("hello");
 console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log(``)
+console.log("hello");
+let a=1;
+let b=2;
+let c = ++a + b++;
+console.log(c);
+console.log(a);
+console.log(b);
+console.log();
