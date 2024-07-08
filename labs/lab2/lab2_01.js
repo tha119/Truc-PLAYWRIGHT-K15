408fcd388b966d11d87ef544c6c6b96112518995
tha <tha@axon.com>
2024-07-08 21:08:12 +0700

    Add lab 2.1

---
+++ labs/lab2/lab2_01.js
@@ -0,0 +1,15 @@
+const reader = require("readline-sync");
+
+let height = reader.question("your height: ");
+let weight = reader.question("your weight: ");
+const bmi = weight/(height*height);
+console.log(`Your BMI: ${bmi}`);
+
+if (bmi < 18.5)
+    console.log("Underweight");
+else if (bmi<= 24.9)
+    console.log ("Normal weight");
+else if (bmi<=29.9 )
+    console.log ("Overweight");
+else
+    console.log("Obesity");
